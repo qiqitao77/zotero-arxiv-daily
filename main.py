6e8f9e75c40e35dc01bff20d2e485161b83a2c39
qiqitao77 <115974922+qiqitao77@users.noreply.github.com>
2025-08-02 18:14:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -63,14 +63,22 @@ def get_arxiv_paper(query:str, debug:bool=False) -> list[ArxivPaper]:
         bar.close()
 
     else:
-        logger.debug("Retrieve 10 arxiv papers regardless of the date.")
-        search = arxiv.Search(query='cat:cs.AI', sort_by=arxiv.SortCriterion.SubmittedDate)
+        # logger.debug("Retrieve 10 arxiv papers regardless of the date.")
+        # search = arxiv.Search(query='cat:cs.AI', sort_by=arxiv.SortCriterion.SubmittedDate)
+        # papers = []
+        # for i in client.results(search):
+        #     papers.append(ArxivPaper(i))
+        #     if len(papers) == 10:
+        #         break
         papers = []
-        for i in client.results(search):
-            papers.append(ArxivPaper(i))
-            if len(papers) == 10:
-                break
-
+        all_paper_ids = [i.id.removeprefix("oai:arXiv.org:") for i in feed.entries if i.arxiv_announce_type == 'new']
+        bar = tqdm(total=len(all_paper_ids),desc="Retrieving Arxiv papers")
+        for i in range(0,len(all_paper_ids),50):
+            search = arxiv.Search(id_list=all_paper_ids[i:i+50])
+            batch = [ArxivPaper(p) for p in client.results(search)]
+            bar.update(len(batch))
+            papers.extend(batch)
+        bar.close()
     return papers
 
 
